2fc35884c8afa83b43433aa81ccee2f1eeb82bf1
Alexis Fox <40548173+foxden09@users.noreply.github.com>
2025-02-28 01:10:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="unified-generative-metrics",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-learn"
+    ],
+    author="Alexis Fox",
+    author_email="alexis.fox@duke.edu",
+    description="Information-theoretic metrics for generative model evaluation",
+    url="https://github.com/NSSAC/PrecisionRecallMetric",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
